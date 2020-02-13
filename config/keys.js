1e72cdc61bc91770da39e5cd8223f7710155e2ad
oloruntobi <awoderuoloruntobi@gmail.com>
2020-02-13 09:24:01 +0100

    Chore: Made changes to key

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 module.exports = {
   TEST_DB: process.env.TEST_DB,
-  DATA_DB: process.env.MONGODB_URI,
+  DATA_DB: process.env.MONGOLAB_URI,
   JWTSecret: process.env.JWT_Secret
 };
