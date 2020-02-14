778af82c81832690aa2bb90cb669c7d79afeffcf
oloruntobi <awoderuoloruntobi@gmail.com>
2020-02-14 12:24:52 +0100

    Chore: Removed redundant code

--- api/routes/searchRoutes.js
+++ api/routes/searchRoutes.js
@@ -5,6 +5,5 @@ const userValidators = require('../validation/userValidation');
 const router = express.Router();
 router.get('/', [userValidators.validateUser], controller.searchUsers);
 router.get('/questions', controller.searchQuestions);
-router.get('/answers', controller.searchAnswers);
 
 module.exports = router;
